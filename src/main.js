de45bc05e96602677d592cfcc816a80e06eeafd4
Said <babaiev.info@gmail.com>
2020-11-20 16:39:25 +0200

    Подключил firebase проекту . сделал авторизацию через firebase

--- src/main.js
+++ src/main.js
@@ -8,14 +8,40 @@ import messagePlugin from './utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip - false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFiler)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const config = {
+  apiKey: "AIzaSyB8gLP6ZkVbhyfbeo6RxpggJU7dPyYveDw",
+  authDomain: "vue-crm-e51ff.firebaseapp.com",
+  databaseURL: "https://vue-crm-e51ff.firebaseio.com",
+  projectId: "vue-crm-e51ff",
+  storageBucket: "vue-crm-e51ff.appspot.com",
+  messagingSenderId: "668345580096",
+  appId: "1:668345580096:web:dc1c9050b56e6f1da93da5",
+  measurementId: "G-T498NQ3QJC"
+}
+firebase.initializeApp(config)
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')  
+  }
+  
+})
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -83,7 +83,7 @@ export default {
     }
   },
   methods: {
-    onSubmit() {
+    async onSubmit() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -94,9 +94,13 @@ export default {
         email: this.email,
         password: this.password
       }
-      console.log(formData)
 
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch(e) {
+      } 
+
     }
   }
 }
