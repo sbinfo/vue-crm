0972b82f199ac38a777cd84ed433f769be71bedb
Said <babaiev.info@gmail.com>
2021-01-07 01:27:41 +0200

    Сделал валидацию формы логина с помошью vuelidate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,6 +10,7 @@ import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuelidate);
 Vue.filter('_date', dateFilter);
 
 new Vue({
