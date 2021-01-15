2b9e446a107c03b6400ac41f4746a6d5a31551c7
Said <babaiev.info@gmail.com>
2021-01-16 01:01:59 +0200

    Сделал регистрацию через firebase

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -96,7 +96,7 @@ export default {
     agree: { checked: v => v }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -108,9 +108,11 @@ export default {
         name: this.name
       }
 
-      console.log(formData);
+      try {
+        await this.$store.dispatch('register', formData);
+        this.$router.push('/');
+      } catch (e) {}
 
-      this.$router.push('/');
     }
   },
 
