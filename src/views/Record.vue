2e018948312b8c6b81c03d50bc0a673779737b5b
Said <babaiev.info@gmail.com>
2021-01-29 20:26:44 +0200

    Оживил страницу добавление записи. Вывел категории из firebase, Сделал валидацию формы

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,38 +4,77 @@
       <h3>Новая запись</h3>
     </div>
 
-    <form class="form">
+    <Loader v-if="loading" />
+
+    <p v-else-if="!categories.length" class="center">
+      Категорий пока нет.
+      <router-link to="categories">Добавить категорию</router-link>
+    </p>
+
+    <form v-else class="form" @submit.prevent="handleSubmit">
       <div class="input-field">
-        <select>
-          <option>name cat</option>
+        <select ref="select" v-model="category">
+          <option v-for="cat in categories" :key="cat.id" :value="cat.id">
+            {{ cat.title }}
+          </option>
         </select>
         <label>Выберите категорию</label>
       </div>
 
       <p>
         <label>
-          <input class="with-gap" name="type" type="radio" value="income" />
+          <input 
+            class="with-gap" 
+            name="type" 
+            type="radio" 
+            value="income"
+            v-model="type"
+          />
           <span>Доход</span>
         </label>
       </p>
 
       <p>
         <label>
-          <input class="with-gap" name="type" type="radio" value="outcome" />
+          <input 
+            class="with-gap"
+            name="type" 
+            type="radio" 
+            value="outcome" 
+            v-model="type"  
+          />
           <span>Расход</span>
         </label>
       </p>
 
       <div class="input-field">
-        <input id="amount" type="number" />
+        <input 
+          id="amount" 
+          type="number" 
+          v-model.number="amount"
+          :class="{ invalid: $v.amount.$dirty && !$v.amount.minValue }"
+        />
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+        <span
+              v-if="$v.amount.$dirty && !$v.amount.minValue"
+              class="helper-text invalid"
+          >Минимальная значение {{ $v.amount.$params.minValue.min }}</span>
       </div>
 
       <div class="input-field">
-        <input id="description" type="text" />
+        <input 
+          id="description"
+          type="text"
+          v-model="description"
+          :class="{ invalid: $v.description.$dirty && !$v.description.minValue }"
+        />
         <label for="description">Описание</label>
-        <span class="helper-text invalid">description пароль</span>
+        <span 
+          v-if="$v.description.$dirty && !$v.description.minValue"
+          class="helper-text invalid"
+        >
+          Введите Описание
+        </span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -45,3 +84,56 @@
     </form>
   </div>
 </template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators';
+
+export default {
+  name: "record",
+  data() {
+    return {
+      loading: true,
+      select: null,
+      categories: [],
+      category: null,
+      type: "outcome",
+      amount: 1,
+      description: ''
+    };
+  },
+  validations: {
+    amount: { minValue: minValue(1) },
+    description: { required }
+  },
+  methods: {
+    handleSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+    }
+  },
+  async mounted() {
+    this.categories = await this.$store.dispatch("fetchCategories");
+    this.loading = false;
+
+    if (this.categories) {
+      this.category = this.categories[0].id;
+    }
+
+    setTimeout(() => {
+      // Инициализация select для materialize
+      this.select = M.FormSelect.init(this.$refs.select);
+
+      // Оживить формы materialize
+      M.updateTextFields();
+    }, 0);
+  },
+  destroyed() {
+    // Очистка плагина для select
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  },
+};
+</script>
